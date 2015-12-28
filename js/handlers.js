edbb0cf66965957b389690b1933aef87d84a2d4c
Ilincescu Mihai <mihai@ilincescu.ro>
2015-12-28 14:13:05 +0200

    Moved the javascript in a namespace

--- js/handlers.js
+++ js/handlers.js
@@ -1,66 +1,58 @@
-﻿$(document).ready(function () {
+﻿App = {};
+App.main_id = null;
 
-
-    function getRef(id) {
-        return "articles/" + id + ".html";           // The function returns the product of p1 and p2
-    }
-    function getID(ref) {
-        return ref;           // The function returns the product of p1 and p2
-    }
+App.initialisation = function () {
     // All sides
-    var sides = ["left",  "right" ];
-   // $("h1 span.version").text($.fn.sidebar.version);
+    var sides = ["left", "right"];
 
     // Initialize sidebars
     for (var i = 0; i < sides.length; ++i) {
         var cSide = sides[i];
         $(".sidebar." + cSide).sidebar({ side: cSide });
     }
-    //create global var
-    var     main_id =null; 
-
 
     //get the first  ul  and save to main
     $("#main_article").empty();
     var $first_article = $('#all_articles ul li div').first();
 
     main_id = $first_article.attr("id");
-   // $("#main_article").load(getRef(main_id));
+    // $("#main_article").load(getRef(main_id));
     $("#main_article").hide().html($first_article.html()).show(500);
     $first_article.empty();
+}
 
+App.ChangeMainArticle = function ($thisElem) {
+    var action = $thisElem.attr("data-action");
+    if (action == "toggle") {
+        var side = $thisElem.attr("data-side");
+        $(".sidebar." + side).trigger("sidebar:" + action);
+    }
+    else {
+        var $currentArticle = $thisElem;
+        var ref = $thisElem.attr("tag");
+        var current_id = $currentArticle.attr("id");
 
-    // Click handlers
-    $(".btn[data-action]").on("click", function () {
-        var $thisElem = $(this);
-        var action = $thisElem.attr("data-action");
-        if (action=="toggle"){
-            var side = $thisElem.attr("data-side");
-            $(".sidebar." + side).trigger("sidebar:" + action);
-        }
-        else {
+        //put the content of main back to its place
+        //get the element
+        $("#" + main_id).empty();
+        $("#" + main_id).hide().html($("#main_article").contents()).show(500);
+        //$("#" + main_id).load(getRef(main_id));
+        // put the selected on into main 
+        $("#main_article").empty();
+        $("#main_article").hide().html($thisElem.contents()).show(500);
+        //$("#main_article").load(ref);
+        main_id = current_id;
+        // empty out the selected one 
+        $currentArticle.empty();
+        App.changeCommentsUrl("http://imafcritic.github.io/articles//");
+    }
+    return false;
+}
 
-            var $currentArticle = $thisElem ;
-            var ref = $thisElem.attr("tag");
-            
-            var current_id = $currentArticle.attr("id");
+App.getRef = function (id) {
+    return "articles/" + id + ".html";
+}
 
-
-
-            //put the content of main back to its place
-            //get the element
-            $("#" + main_id).empty();
-            $("#" + main_id).hide().html($("#main_article").contents()).show(500);
-            //$("#" + main_id).load(getRef(main_id));
-            // put the selected on into main 
-            $("#main_article").empty();
-            $("#main_article").hide().html($thisElem.contents()).show(500);
-            //$("#main_article").load(ref);
-            main_id = current_id; 
-            // empty out the selected one 
-            $currentArticle.empty();
-            changeCommentsUrl(main_id); 
-        }
-        return false;
-    });
-});
+App.getID = function (ref) {
+    return ref;
+}
