b79587ca13a9015f4c5e4e1a2537d9ebd774f72d
Ilincescu Mihai <mihai@ilincescu.ro>
2015-12-30 11:07:10 +0200

    Revert " facebook link bug"
    This reverts commit 3fbf4adbea1fbd5f3ffbf74798d8d1866f53b7b5.

--- js/facebookLogic.js
+++ js/facebookLogic.js
@@ -1,7 +1,5 @@
 ﻿ 
-App.changeCommentsUrl = function (newUrl) {
-
-    newUrl = newUrl.substring(1);
+App.changeCommentsUrl = function(newUrl) {
     // should refresh fb comments plugin for the "newUrl" variable
     $(".fb-comments").attr("data-href", $(".fb-comments").attr("tag")+newUrl);//
      FB.XFBML.parse(document.getElementById('comments'));
